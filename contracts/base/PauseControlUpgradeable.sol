c6e8da52a891d1fccc8b4a14572abecd9bf21c0e
IgorSenychCW <igor.senych@cloudwalk.io>
2022-08-11 22:02:07 +0300

    Reserve storage space on PauseControlUpgradeable contract

--- contracts/base/PauseControlUpgradeable.sol
+++ contracts/base/PauseControlUpgradeable.sol
@@ -53,4 +53,10 @@ abstract contract PauseControlUpgradeable is AccessControlUpgradeable, PausableU
     function unpause() external onlyRole(PAUSER_ROLE) {
         _unpause();
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     */
+    uint256[50] private __gap;
 }
