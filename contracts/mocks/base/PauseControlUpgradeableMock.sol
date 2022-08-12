9aed7338b5baf0374636f852335f8db1c01ecb44
IgorSenychCW <igor.senych@cloudwalk.io>
2022-08-12 03:24:01 +0300

    Rename PausableExUpgradeableMock -> PauseControlUpgradeableMock

--- contracts/mocks/base/PauseControlUpgradeableMock.sol
+++ contracts/mocks/base/PauseControlUpgradeableMock.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.8;
 
-import "../../base/PauseControlUpgradeable.sol";
+import { PauseControlUpgradeable } from "../../base/PauseControlUpgradeable.sol";
 
 /**
  * @title PauseControlUpgradeableMock contract
